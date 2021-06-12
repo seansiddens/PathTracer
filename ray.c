a15c9e0597cfd4bb38b21f48aeda6c7c223a653a
Sean Siddens <seansiddens@gmail.com>
2021-06-12 16:32:47 -0700

    Implemented ray_at() function

---
+++ ray.c
@@ -0,0 +1,7 @@
+#include "ray.h"
+
+// Returns the point at t along the ray r
+// R(t) = orig + t * dir
+double ray_at(ray r, double t) {
+    vec3 p = v3_add(r.orig, v3_scale(r.dir, t));
+}
--- ray.h
+++ ray.h
@@ -2,6 +2,8 @@
 
 #include "vec3.h"
 
+// Struct definition for a ray in 3D space. 
+// Defined by it's origin and direction.
 typedef struct {
     vec3 orig;
     vec3 dir;
