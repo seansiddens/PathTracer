5390254877853392ffc440063945aceb8a87cdbc
Sean Siddens <seansiddens@gmail.com>
2021-06-12 19:56:12 -0700

    Did a clang-format

--- hittable.h
+++ hittable.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include "vec3.h"
 #include "ray.h"
+#include "vec3.h"
 
-// Struct definition for a hit record. Records the point at which the hit occurred,
-// surface normal of the hit, and the t value of the hit
+// Struct definition for a hit record. Records the point at which the hit
+// occurred, surface normal of the hit, and the t value of the hit
 typedef struct {
     vec3 p;
     vec3 normal;
