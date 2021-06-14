4ff3df22672f0e92907a5b5a4fedb0f15c260a36
Sean Siddens <seansiddens@gmail.com>
2021-06-14 14:29:47 -0700

    Added metal material

--- material.c
+++ material.c
@@ -16,6 +16,10 @@ typedef struct {
     color albedo;
 } Lambertian;
 
+typedef struct {
+    color albedo;
+} Metal;
+
 //
 // Returns a pointer to a newly created Lambertian material.
 // The albedo is roughly the color of the material.
@@ -33,6 +37,26 @@ Material *create_lambertian(color albedo) {
     lambert->albedo = albedo;
 
     mat->material = lambert;
+
+    return mat;
+}
+
+//
+// Returns a pointer to a newly created metal material. 
+// Metal reflects incoming rays and has a surface albedo. 
+//
+Material *create_metal(color albed) {
+    Material *mat = (Material *)malloc(sizeof(Material));
+    assert(mat != NULL);
+
+    // Set material type to metal
+    mat->type = METAL;
+
+    // Initialize the material w/ supplied albedo
+    Metal *metal = (Metal *)malloc(sizeof(Metal));
+    assert(metal != NULL);
+
+    mat->material = metal;
 
     return mat;
 }
@@ -70,13 +94,13 @@ bool scatter(Material *mat, ray ray_in, HitRecord *rec, color *attenuation,
         if (v3_near_zero(scatter_direction)) {
             scatter_direction = rec->normal;
         }
-
         ray_scattered->orig = rec->p;
         ray_scattered->dir = scatter_direction;
         *attenuation = ((Lambertian *)(mat->material))->albedo;
         return true;
     } else if (mat->type == METAL) {
-
+        v3_print(ray_in.orig);
+        return false;
     } else {
         fprintf(stderr,
                 "ERROR: Unknown material type encountered in scatter()!\n");
--- material.h
+++ material.h
@@ -10,6 +10,8 @@ typedef struct Material Material;
 
 Material *create_lambertian(color albedo);
 
+Material *create_metal(color albedo);
+
 void mat_delete(Material **mat);
 
 bool scatter(Material *mat, ray ray_in, HitRecord *rec, color *attenuation,
