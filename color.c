f0f18b1c288d23c6155171a576382cca18d84975
Sean Siddens <seansiddens@gmail.com>
2021-06-13 20:59:48 -0700

    Doin some gamma correction

--- color.c
+++ color.c
@@ -1,12 +1,18 @@
 #include "color.h"
 #include "util.h"
 
+#include <math.h>
+
 // Writes out the color of the pixel given an index into the image buffer
 void write_color(uint8_t *image, color pixel_color, uint32_t i,
                  uint32_t samples_per_pixel) {
 
-    // Scale color based on # of samples taken per pixel
-    pixel_color = v3_scale(pixel_color, 1.0 / (double)samples_per_pixel);
+    // Scale color based on the number of samples taken per pixel and
+    // gamma-correct for gamma=2.0.
+    double scale = 1.0 / (double)samples_per_pixel;
+    pixel_color.x = sqrt(scale * pixel_color.x);
+    pixel_color.y = sqrt(scale * pixel_color.y);
+    pixel_color.z = sqrt(scale * pixel_color.z);
 
     // Set value of color channels at the pixel
     image[i] = (uint8_t)(255 * clamp(pixel_color.x, 0.0, 1.0));
