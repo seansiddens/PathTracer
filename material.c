4fe19529d1cc1f00ea1a51eb3998f0d05fe82541
Sean Siddens <seansiddens@gmail.com>
2021-06-14 11:21:58 -0700

    Implemented lambertian material type

--- material.c
+++ material.c
@@ -1,1 +1,41 @@
 #include "material.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//
+// Returns a pointer to a newly created Lambertian material.
+// The albedo is roughly the color of the material.
+//
+Material *create_lambertian(color albedo) {
+    Material *mat = (Material *)malloc(sizeof(Material));
+    assert(mat != NULL);
+
+    // Set the material type to Lambertian
+    mat->type = LAMBERTIAN;
+
+    // Initialize the material w/ the supplied albedo
+    Lambertian *lambert = (Lambertian *)malloc(sizeof(Lambertian));
+    assert(lambert != NULL);
+    lambert->albedo = albedo;
+
+    mat->material = lambert;
+
+    return mat;
+}
+
+//
+// Deallocates material memory.
+//
+void mat_delete(Material **mat) {
+    if (*mat) {
+        if ((*mat)->material) {
+            free((*mat)->material);
+            (*mat)->material = NULL;
+        }
+        free(*mat);
+        *mat = NULL;
+    }
+    return;
+}
--- material.h
+++ material.h
@@ -1,9 +1,19 @@
 #pragma once
 
+#include "vec3.h"
+
 enum MaterialType { LAMBERTIAN, METAL };
 typedef enum MaterialType MaterialType;
 
 typedef struct {
     MaterialType type;
-
+    void *material;
 } Material;
+
+typedef struct {
+    color albedo;
+} Lambertian;
+
+Material *create_lambertian(color albedo);
+
+void mat_delete(Material **mat);
