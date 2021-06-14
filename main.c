2ff8a5475845d7fb99065026d244fc9785b081d1
Sean Siddens <seansiddens@gmail.com>
2021-06-13 20:19:53 -0700

    Taking multiple samples per pixel

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include "camera.h"
+#include "color.h"
 #include "hit.h"
 #include "ray.h"
 #include "scene.h"
 #include "sphere.h"
 #include "vec3.h"
+#include "util.h"
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -38,6 +40,7 @@ int main(void) {
     // Buffer for storing image data
     uint8_t *image =
         (uint8_t *)calloc(image_width * image_height * 3, sizeof(uint8_t));
+    uint32_t samples_per_pixel = 100;
 
     // Camera settings
     Camera *cam = cam_create(v3_init(0, 0, 0), aspect_ratio, 1.0);
@@ -57,19 +60,23 @@ int main(void) {
             // Get index into buffer from x and y coordinates
             uint32_t i = y * image_width * 3 + x * 3;
 
-            // Map image coordinates to normalized (u, v) coordinates
-            double u = x / (double)(image_width - 1);
-            double v = 1.0 - (y / (double)(image_height - 1));
+            // Take multiple samples per pixel
+            color pixel_color = v3_init(0, 0, 0);
+            for (uint32_t s = 0; s < samples_per_pixel; s++) {
+                // Map image coordinates to normalized (u, v) coordinates, 
+                // offset by random amount for antialiasing
+                double u = (double)(x + random_uniform()) / (image_width - 1);
+                double v = 1.0 - ((double)(y + random_uniform()) / (image_height - 1));
+                
+                // Get view ray from camera to viewport
+                ray view_ray = get_view_ray(cam, u, v);
 
-            ray view_ray = get_view_ray(cam, u, v);
-
-            // Get color of what ray is looking at
-            color col = ray_color(view_ray, scene);
-
-            // Set value of color channels at the pixel
-            image[i] = (uint8_t)(255 * col.x);
-            image[i + 1] = (uint8_t)(255 * col.y);
-            image[i + 2] = (uint8_t)(255 * col.z);
+                // Accumulate color of what ray is looking at
+                pixel_color = v3_add(pixel_color, ray_color(view_ray, scene));
+               
+            }
+            // Write color to final image 
+            write_color(image, pixel_color , i, samples_per_pixel);
         }
     }
     printf("Done!\n");
