696450e990015273ac58b29d76e0d9a4726b1423
Sean Siddens <seansiddens@gmail.com>
2021-06-13 20:21:10 -0700

    Did a clang-format

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 #include "ray.h"
 #include "scene.h"
 #include "sphere.h"
-#include "vec3.h"
 #include "util.h"
+#include "vec3.h"
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -63,20 +63,20 @@ int main(void) {
             // Take multiple samples per pixel
             color pixel_color = v3_init(0, 0, 0);
             for (uint32_t s = 0; s < samples_per_pixel; s++) {
-                // Map image coordinates to normalized (u, v) coordinates, 
+                // Map image coordinates to normalized (u, v) coordinates,
                 // offset by random amount for antialiasing
                 double u = (double)(x + random_uniform()) / (image_width - 1);
-                double v = 1.0 - ((double)(y + random_uniform()) / (image_height - 1));
-                
+                double v =
+                    1.0 - ((double)(y + random_uniform()) / (image_height - 1));
+
                 // Get view ray from camera to viewport
                 ray view_ray = get_view_ray(cam, u, v);
 
                 // Accumulate color of what ray is looking at
                 pixel_color = v3_add(pixel_color, ray_color(view_ray, scene));
-               
             }
-            // Write color to final image 
-            write_color(image, pixel_color , i, samples_per_pixel);
+            // Write color to final image
+            write_color(image, pixel_color, i, samples_per_pixel);
         }
     }
     printf("Done!\n");
