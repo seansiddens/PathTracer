a424afce323282013c32545a256cc20daf1a6e17
Sean Siddens <seansiddens@gmail.com>
2021-06-14 22:36:08 -0700

    Implemented depth of field

--- camera.c
+++ camera.c
@@ -10,7 +10,7 @@
 // degrees.
 //
 Camera *cam_create(vec3 vup, vec3 look_from, vec3 look_at, double aspect_ratio,
-                   double vfov) {
+                   double vfov, double aperture, double focus_dist) {
     Camera *cam = (Camera *)malloc(sizeof(Camera));
     assert(cam != NULL);
 
@@ -22,17 +22,21 @@ Camera *cam_create(vec3 vup, vec3 look_from, vec3 look_at, double aspect_ratio,
     cam->viewport_height = 2.0 * h;
     cam->viewport_width = cam->aspect_ratio * cam->viewport_height;
 
-    vec3 w = v3_unit_vector(v3_sub(look_from, look_at));
-    vec3 u = v3_unit_vector(v3_cross(vup, w));
-    vec3 v = v3_cross(w, u);
+    cam->w = v3_unit_vector(v3_sub(look_from, look_at));
+    cam->u = v3_unit_vector(v3_cross(vup, cam->w));
+    cam->v = v3_cross(cam->w, cam->u);
 
+    cam->focus_dist = focus_dist;
+    cam->aperture = aperture;
     cam->origin = look_from;
-    cam->horizontal = v3_scale(u, cam->viewport_width);
-    cam->vertical = v3_scale(v, cam->viewport_height);
+    cam->horizontal = v3_scale(cam->u, cam->viewport_width * cam->focus_dist);
+    cam->vertical = v3_scale(cam->v, cam->viewport_height * cam->focus_dist);
     cam->lower_left_corner =
         v3_sub(v3_sub(v3_sub(cam->origin, v3_scale(cam->horizontal, 0.5)),
                       v3_scale(cam->vertical, 0.5)),
-               w);
+               v3_scale(cam->w, cam->focus_dist));
+    cam->lens_radius = aperture / 2;
+
     return cam;
 }
 
@@ -48,15 +52,20 @@ void cam_delete(Camera **cam) {
 }
 
 //
-// Given normalized (u, v) coordinates, returns the view ray from the camera
+// Given normalized (s, t) coordinates, returns the view ray from the camera
 // origin to the viewport.
 //
-ray get_view_ray(Camera *cam, double u, double v) {
+ray get_view_ray(Camera *cam, double s, double t) {
+    vec3 rd = v3_scale(random_in_unit_disk(), cam->lens_radius);
+    vec3 offset = v3_add(v3_scale(cam->u, rd.x), v3_scale(cam->v, rd.y));
+
     // Get direction from camera origin to point on viewport
-    vec3 dir = v3_add(v3_add(cam->lower_left_corner, v3_scale(cam->horizontal, u)),
-                      v3_scale(cam->vertical, v));
+    vec3 dir = v3_add(cam->lower_left_corner, v3_scale(cam->horizontal, s));
+    dir = v3_add(dir, v3_scale(cam->vertical, t));
+    dir = v3_sub(dir, cam->origin);
+    dir = v3_sub(dir, offset);
 
     // View ray
-    ray r = {cam->origin, dir};
+    ray r = {v3_add(cam->origin, offset), dir};
     return r;
 }
--- camera.h
+++ camera.h
@@ -4,14 +4,14 @@
 #include "vec3.h"
 
 typedef struct {
-    double aspect_ratio, vfov;
+    double aspect_ratio, vfov, aperture, focus_dist;
     vec3 origin;
-    double viewport_height, viewport_width;
-    vec3 horizontal, vertical, lower_left_corner;
+    double viewport_height, viewport_width, lens_radius;
+    vec3 u, v, w, horizontal, vertical, lower_left_corner;
 } Camera;
 
 Camera *cam_create(vec3 vup, vec3 look_from, vec3 look_at, double aspect_ratio,
-                   double vfov);
+                   double vfov, double aperture, double focus_dist);
 
 void cam_delete(Camera **cam);
 
