02ac3f08273ef8059d68020fbfad704b41dc3d08
Sean Siddens <seansiddens@gmail.com>
2021-06-16 14:12:43 -0700

    Implemented hittable_print()

--- hittable.c
+++ hittable.c
@@ -21,7 +21,7 @@ Hittable *hittable_create(void *object, HittableType type) {
 //
 // Destructor for a hittable.
 //
-void node_delete(Hittable **hittable) {
+void hittable_delete(Hittable **hittable) {
     if (*hittable) {
         if ((*hittable)->type == SPHERE) {
             Sphere *s = (Sphere *)(*hittable)->object;
@@ -31,4 +31,20 @@ void node_delete(Hittable **hittable) {
         *hittable = NULL;
     }
     return;
+}
+
+// 
+// Prints a hittable struct - calls the corresponding print function of the object type.
+//
+void hittable_print(Hittable *h) {
+    if (h) {
+        switch (h->type) {
+            case SPHERE:
+                sphere_print((Sphere *)h->object);
+                break;
+            default:
+                fprintf(stderr, "ERRROR: Unknown object type encountered in hittable_print()!\n");
+                break;
+        }
+    }
 }
--- hittable.h
+++ hittable.h
@@ -13,3 +13,5 @@ struct Hittable {
 Hittable *hittable_create(void *object, HittableType type);
 
 void hittable_delete(Hittable **n);
+
+void hittable_print(Hittable *h);
