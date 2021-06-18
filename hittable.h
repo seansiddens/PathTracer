d242d0ebaf3ff64a341d19c21e3297506695fb5f
Sean Siddens <seansiddens@gmail.com>
2021-06-17 19:36:24 -0700

    Implemented hittable_intersect()

--- hittable.h
+++ hittable.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "aabb.h"
+#include "hit.h"
+
 enum HittableType { SPHERE };
 typedef enum HittableType HittableType;
 
@@ -11,6 +14,10 @@ struct Hittable {
 };
 
 Hittable *hittable_create(void *object, HittableType type);
+
+bool hittable_intersect(Hittable h, ray r, double t_min, double t_max, HitRecord *rec);
+
+bool hittable_bounding_box(Hittable h, AABB *output_box);
 
 void hittable_delete(Hittable **n);
 
