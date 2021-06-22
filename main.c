c53f2f18fcd6fe34458cffdce0fafc891e4c1e91
Sean Siddens <seansiddens@gmail.com>
2021-06-21 17:39:54 -0700

    Implemented multithreading

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #include "util.h"
 #include "vec3.h"
 
+#include <pthread.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -17,6 +18,20 @@
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "include/stb_image/stb_image_write.h"
+
+#define MULITHREAD true
+#define NUM_THREADS 8
+
+
+
+typedef struct {
+    Scene *scene;
+    Camera *cam;
+    BVHNode *bvh;
+    uint32_t image_width, image_height, samples_per_pixel, max_depth;
+    uint8_t *image;
+    uint32_t thread_id;
+} RenderArgs;
 
 // TODO: Every time an object is added to the scene, keep track of it's material
 // using a LL, then when scene_delete() is called, free that memory.
@@ -26,12 +41,13 @@
 //
 color ray_color(Scene *scene, BVHNode *bvh, ray r, uint32_t depth) {
     HitRecord rec;
+    rec.t = INFINITY;
 
     // If we've exceeded the ray bounce limit, no more light is gathered.
     if (depth == 0) {
         return v3_init(0, 0, 0);
     }
-
+        
     // Check if ray hits an object in our scene
     if (bvh_hit(bvh, r, 0.001, INFINITY, &rec)) {
         ray scattered;
@@ -50,6 +66,40 @@ color ray_color(Scene *scene, BVHNode *bvh, ray r, uint32_t depth) {
     return v3_lerp(start_color, end_color, t);
 }
 
+void *render(void *thread_args) {
+    RenderArgs *args = (RenderArgs *)thread_args;
+
+    printf("Thread %d start!\n", args->thread_id);
+
+    // Render chunk of image buffer from start_idx to end_idx
+    for (uint32_t i = args->thread_id * 3; i <= args->image_width * args->image_height * 3; i += (NUM_THREADS * 3)) {
+        uint32_t x = (i / 3) % args->image_width;
+        uint32_t y = i / args->image_width / 3;
+
+        // Take multiple samples per pixel
+        color pixel_color = v3_init(0, 0, 0);
+        for (uint32_t s = 0; s < args->samples_per_pixel; s++) {
+            // Map image coordinates to normalized (u, v) coordinates,
+            // offset by random amount for antialiasing
+            double u = (double)(x + random_uniform()) / (args->image_width - 1);
+            double v = 1.0 - ((double)(y + random_uniform()) / (args->image_height - 1));
+
+            // Get view ray from camera to viewport
+            ray view_ray = get_view_ray(args->cam, u, v);
+
+            // Accumulate color of what ray is looking at
+            pixel_color =
+                v3_add(pixel_color, ray_color(args->scene, args->bvh, view_ray, args->max_depth));
+        }
+        // Write color to final image
+        write_color(args->image, pixel_color, i, args->samples_per_pixel);
+    }
+
+    printf("Thread %d done!\n", args->thread_id);
+
+    return NULL;
+}
+
 int main(void) {
     // Image Settings
     const double aspect_ratio = 3.0 / 2.0;
@@ -62,52 +112,86 @@ int main(void) {
 
     // Camera settings
     vec3 vup = v3_init(0, 1, 0);
-    vec3 look_from = v3_init(0, 2, 2);
-    vec3 look_at = v3_init(0, 0, -1);
+    vec3 look_from = v3_init(0, 2, 10);
+    vec3 look_at = v3_init(0, 1, -1);
     double dist_to_focus = v3_length(v3_sub(look_from, look_at));
-    double aperture = 0.1;
+    double aperture = 0.01;
+    double vfov = 45;
     Camera *cam =
-        cam_create(vup, look_from, look_at, aspect_ratio, 65, aperture, dist_to_focus);
+        cam_create(vup, look_from, look_at, aspect_ratio, vfov, aperture, dist_to_focus);
 
     // Scene settings
-    /*Scene *scene = random_scene();*/
-    Scene *scene = scene_create();
-
-    Material *mat = create_lambertian(v3_init(0.8, 0.8, 0.9));
-    scene_add_sphere(scene, 0, 1, -1, 1.0, mat);
-
+    Scene *scene = random_scene();
+    // Construct BVH
     BVHNode *bvh = bvh_create(scene, 0, scene->object_count - 1);
 
-    // Iterate over each pixel in the image
-    for (uint32_t y = 0; y < image_height; y++) {
-        // Print progress
-        if (y % 50 == 0) {
-            printf("Rows remaining: %d\n", image_height - y);
+    if (MULITHREAD) {
+        // Thread setup
+        pthread_t threads[NUM_THREADS];
+        RenderArgs thread_args[NUM_THREADS];
+
+        // Initialize thread creation arguments for each thread
+        for (int thread = 0; thread < NUM_THREADS; thread++) {
+            thread_args[thread].bvh = bvh;
+            thread_args[thread].cam = cam;
+            thread_args[thread].scene = scene;
+            thread_args[thread].image_width = image_width;
+            thread_args[thread].image_height = image_height;
+            thread_args[thread].samples_per_pixel = samples_per_pixel;
+            thread_args[thread].max_depth = max_depth;
+            thread_args[thread].image = image;
+            thread_args[thread].thread_id = thread;
         }
-        for (uint32_t x = 0; x < image_width; x++) {
-            // Get index into buffer from x and y coordinates
-            uint32_t i = y * image_width * 3 + x * 3;
 
-            // Take multiple samples per pixel
-            color pixel_color = v3_init(0, 0, 0);
-            for (uint32_t s = 0; s < samples_per_pixel; s++) {
-                // Map image coordinates to normalized (u, v) coordinates,
-                // offset by random amount for antialiasing
-                double u = (double)(x + random_uniform()) / (image_width - 1);
-                double v = 1.0 - ((double)(y + random_uniform()) / (image_height - 1));
-
-                // Get view ray from camera to viewport
-                ray view_ray = get_view_ray(cam, u, v);
-
-                // Accumulate color of what ray is looking at
-                pixel_color =
-                    v3_add(pixel_color, ray_color(scene, bvh, view_ray, max_depth));
+        // Spawn each thread
+        for (int thread = 0; thread < NUM_THREADS; thread++) {
+            int rc = pthread_create(&threads[thread], NULL, render, (void *)&thread_args[thread]);
+            if (rc) {
+                fprintf(stderr, "ERROR: Return code from pthread_create() is %d\n", rc);
+                exit(1);
             }
-            // Write color to final image
-            write_color(image, pixel_color, i, samples_per_pixel);
         }
+
+        // Join threads
+        for (int thread = 0; thread < NUM_THREADS; thread++) {
+            pthread_join(threads[thread], NULL);
+        }
+
+    } else {
+        // Iterate over each pixel in the image
+        for (uint32_t y = 0; y < image_height; y++) {
+            // Print progress
+            if (y % 50 == 0) {
+                printf("Rows remaining: %d\n", image_height - y);
+            }
+            for (uint32_t x = 0; x < image_width; x++) {
+                // Get index into buffer from x and y coordinates
+                uint32_t i = y * image_width * 3 + x * 3;
+
+                // Take multiple samples per pixel
+                color pixel_color = v3_init(0, 0, 0);
+                for (uint32_t s = 0; s < samples_per_pixel; s++) {
+                    // Map image coordinates to normalized (u, v) coordinates,
+                    // offset by random amount for antialiasing
+                    double u = (double)(x + random_uniform()) / (image_width - 1);
+                    double v = 1.0 - ((double)(y + random_uniform()) / (image_height - 1));
+
+                    // Get view ray from camera to viewport
+                    ray view_ray = get_view_ray(cam, u, v);
+
+                    // Accumulate color of what ray is looking at
+                    pixel_color =
+                        v3_add(pixel_color, ray_color(scene, bvh, view_ray, max_depth));
+                }
+                // Write color to final image
+                write_color(image, pixel_color, i, samples_per_pixel);
+            }
+        }
+
     }
-    printf("Done!\n");
+
+
+
 
     // Write contents of image buffer out to PNG
     char *image_name = "out.png";
